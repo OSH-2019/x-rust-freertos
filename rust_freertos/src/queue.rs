2db37943c6ddf0129dcbeea65af73a4a57a02400
nnnyt <793313994@qq.com>
2019-04-23 21:03:02 +0800

    Update queue.rs

---
+++ rust_freertos/src/queue.rs
@@ -0,0 +1,286 @@
+use std::collections::VecDeque;
+use crate::list::*;
+use crate::port::*;
+//use volatile::Volatile;
+//
+pub const queueUNLOCKED:i8 = -1;
+pub const queueLOCKED_UNMODIFIED:i8 = 0;
+pub const queueSEND_TO_BACK:BaseType = 0;
+
+type UBaseType = u64;
+type BaseType = i64;
+
+pub enum QueueUnion {
+    pcReadFrom(UBaseType),
+    uxRecuriveCallCount(UBaseType),
+}
+
+pub struct QueueDefinition<T>{
+    pcQueue: VecDeque<T>,
+    
+    pcHead: UBaseType,
+    pcTail: UBaseType,
+    pcWriteTo: UBaseType,
+
+    u: QueueUnion,
+
+    xTaskWaitingToSend:& Vec<Rc<RefCell<ListItem>>>,
+    xTaskWaitingToReceive: ListItem,
+
+    uxMessagesWaiting: UBaseType,
+    uxLength: UBaseType,
+    uxItemSize: UBaseType,  //这玩意还有必要吗
+    
+    cRxLock: i8,
+    cTxLock: i8,
+    
+    #[cfg(all(configSUPPORT_STATIC_ALLOCATION, configSUPPORT_DYNAMIC_ALLOCATION))]
+    ucStaticallyAllocated: u8,
+    
+    #[cfg(configUSE_QUEUE_SETS)]
+    pxQueueSetContainer:Option<Box<QueueDefinition>>,
+    
+    #[cfg(configUSE_TRACE_FACILITY)] 
+    uxQueueNumber: UBaseType,
+    #[cfg(configUSE_TRACE_FACILITY)]
+    ucQueueType: u8,
+
+}
+type xQueue<T> = QueueDefinition<T>;
+type Queue<T> = QueueDefinition<T>;
+
+
+
+impl <T>QueueDefinition<T>{
+
+    #[cfg(configSUPPORT_DYNAMIC_ALLOCATION)]
+    fn xQueueGenericCreate<T>(&self, uxQueueLength:UBaseType, uxItem:T, ucQueueType:u8) -> Queue<T> {
+    
+        let NewQueue = Queue {
+            pcQueue: VecDeque::with_capacity(uxQueueLength)
+        };
+        
+        #[cfg(configSUPPORT_STATIC_ALLOCATION)]
+        self.ucStaticallyAllocated = false;
+        
+        NewQueue
+    }
+
+    fn prvInitialiseNewQueue(&self, uxQueueLength: UBaseType, ucQueueType: u8)  {
+        self. pcHead = 0;
+        self.uxLength = uxQueueLength;
+        self.xQueueGenericReset(true);
+
+        if cfg!(configUSE_TRACE_FACILITY) {
+        self.ucQueueType = ucQueueType;}
+        #[cfg(configUSE_QUEUE_SETS)]
+        self.pxQueueSetContainer  = None;
+
+        traceQUEUE_CREATE!(self);
+    }
+
+
+    fn xQueueGenericReset(&self, xNewQueue: bool) -> bool{
+        //xNewQueue源码中为BaseType，改为bool
+        //返回值原为BaseType，改为bool
+        taskENTER_CRITICAL();
+        {
+            //初始化队列相关成员变量
+            self.pcTail = self.pcHead + self.uxLength;
+            self.uxMessagesWaiting = 0 as UBaseType;
+            self.pcWriteTo = self.pcHead;
+            self.pcReadFrom = self.pcHead + self.uxLength - (1 as UBaseType);
+            self.cRxLock = queueUNLOCKED;
+            self.cTxLock = queueUNLOCKED;
+            self.pcQueue.clear();//初始化空队列
+            if xNewQueue == false {
+                if list_is_empty!( &(self.xTasksWaitingToSend)) == false {
+                    if xTaskRemoveFromEventList( &(self.xTasksWaitingToSend)) != false{
+                        queueYIELD_IF_USING_PREEMPTION();
+                    }
+                    else{
+                        mtCOVERAGE_TEST_MARKER!();
+                    }
+                }
+                else{
+                    mtCOVERAGE_TEST_MARKER!();
+                }
+            }
+            else{
+                list_initialise!(&(self.xTasksWaitingToSend));
+                list_initialise!(&(self.xTasksWaitingToReceive));
+            }
+        }
+        taskEXIT_CRITICAL();
+        true
+    }
+
+    fn xQueueGenericSend(&self, pvItemToQueue: T, xTicksToWait: TickType, xCopyPosition: BaseType) {
+        let xEntryTimeSet: bool = false;
+        let xYieldRequired: BaseType;
+        let xTimeOut: TimeOut;
+    }
+
+    fn xQueueGenericSendFromISR(&self, pvItemToQueue: T, xCopyPosition: BaseType) ->(BaseType, bool){
+        //原先参数const pxHigherPriorityTaskWoken: BaseType作为返回值的第二个元素，bool型
+        //返回值改为struct
+
+        let xReturn: BaseType;
+        let pxHigherPriorityTaskWoken:bool = false;//默认为false,下面一些情况改为true
+
+        //少了三句assert
+
+        portASSERT_IF_INTERRUPT_PRIORITY_INVALID!();
+        let uxSavedInterruptStatus: UBaseType = portSET_INTERRUPT_MASK_FROM_ISR!();
+        {
+            if self.uxMessagesWaiting < self.uxLength || xCopyPosition == queueOVERWRITE {
+
+                let cTxLock: i8 = self.cTxLock;
+                traceQUEUE_SEND_FROM_ISR! (self);
+                prvCopyDataToQueue(self, pvItemToQueue, xCopyPosition);
+
+                if cTxLock == queueUNLOCKED{
+
+                    #[cfg(configUSE_QUEUE_SETS)]
+                    match self.pxQueueSetContainer{
+                        Some =>{
+                            if prvNotifyQueueSetContainer(self, xCopyPosition ) != false{
+                                pxHigherPriorityTaskWoken = true
+                            }
+                            else {
+                                mtCOVERAGE_TEST_MARKER!();
+                            }
+                        }
+                        None => {
+                            if list_is_empty!( &self.xTasksWaitingToReceive ) == false{
+                                if xTaskRemoveFromEventList( &self.xTasksWaitingToReceive ) != false{
+                                    pxHigherPriorityTaskWoken = true;
+                                }
+                                else {
+                                    mtCOVERAGE_TEST_MARKER!();
+                                }
+                            }
+                            else {
+                                mtCOVERAGE_TEST_MARKER!();
+                            }
+                        }
+                    }
+
+                    if cfg!(not(configUSE_QUEUE_SETS)) {
+                        if list_is_empty!(&self.xTasksWaitingToReceive) == false{
+                            if xTaskRemoveFromEventList( &self.xTasksWaitingToReceive) != false{
+                                pxHigherPriorityTaskWoken = true;
+                            }
+                            else {
+                                mtCOVERAGE_TEST_MARKER!();
+                            }
+                        }
+                        else {
+                            mtCOVERAGE_TEST_MARKER!();
+                        }
+                    }
+                }
+                else {
+                    self.cTxLock = (cTxLock + 1) as i8;
+                }
+                xReturn = pdPASS;
+            }
+            else {
+                traceQueue_SEND_FROM_ISR_FAILED!(self);
+                xReturn = errQUEUE_FULL;
+            }
+        }
+        portCLEAR_INTERRUPT_MASK_FROM_ISR!( uxSavedInterruptStatus );
+        (xReturn,pxHigherPriorityTaskWoken)
+   }
+
+   fn prvLockQueue (&self){
+    //源码中为宏，改为Queue的方法
+    taskENTER_CRITICAL();
+    {
+        if self.cRxLock == queueUNLOCKED{
+            self.cRxLock = queueLOCKED_UNMODIFIED;
+        }
+        if self.cTxLock == queueUNLOCKED{
+            self.cTxLock = queueLOCKED_UNMODIFIED;
+        }
+    }
+    taskEXIT_CRITICAL();
+   }
+
+   fn prvUnlockQueue (&self){
+
+        taskENTER_CRITICAL();
+        {
+            let cTxLock:i8 = self.cTxLock;
+            while cTxLock > queueLOCKED_UNMODIFIED{
+
+                #[cfg(configUSE_QUEUE_SETS)]
+                match self.pxQueueSetContainer{
+                    Some =>{
+                        if prvNotifyQueueSetContainer(self, queueSEND_TO_BACK) != false{
+                            vTaskMissedYield();
+                        }
+                        else {
+                            mtCOVERAGE_TEST_MARKER!();
+                        }
+                    }
+                    None =>{
+                        if list_is_empty!(&self.xTasksWaitingToReceive) == false{
+                            if xTaskRemoveFromEventList( &self.xTasksWaitingToReceive) != false{
+                                vTaskMissedYield();
+                            }
+                            else {
+                                mtCOVERAGE_TEST_MARKER!();
+                            }
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                }
+
+                if cfg!(not(configUSE_QUEUE_SETS)) {
+                    if list_is_empty!(&self.xTasksWaitingToReceive) == false{
+                        if xTaskRemoveFromEventList( &self.xTasksWaitingToReceive) != false{
+                            vTaskMissedYield();
+                        }
+                        else {
+                            mtCOVERAGE_TEST_MARKER!();
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+
+                --cTxLock;
+            }
+            self.cTxLock == queueUNLOCKED;
+        }
+        taskEXIT_CRITICAL();
+
+        taskENTER_CRITICAL();
+        {
+            let cRxLock:i8 = self.cRxLock;
+            while cRxLock > queueLOCKED_UNMODIFIED{
+                if list_is_empty!(&self.xTasksWaitingToReceive) == false{
+                    if xTaskRemoveFromEventList(&self.xTasksWaitingToReceive) != false{
+                        vTaskMissedYield();
+                    }
+                    else {
+                        mtCOVERAGE_TEST_MARKER!();
+                    }
+
+                    --cRxLock;
+                }
+                else {
+                    break;
+                }
+            }
+            self.cRxLock = queueUNLOCKED;
+        }
+        taskEXIT_CRITICAL();
+    }
+
+}
