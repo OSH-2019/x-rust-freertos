2186ccc6d42c1ad390669a285c877ee7d7444f50
nnnyt <793313994@qq.com>
2019-05-07 21:26:57 +0800

    add queue.rs and queue_h.rs

---
+++ rust_freertos/src/queue_h.rs
@@ -0,0 +1,14 @@
+use crate::port::*;
+
+pub const queueSEND_TO_BACK: BaseType = 0 ;
+pub const queueSEND_TO_FRONTi:BaseType = 1;
+pub const queueOVERWRITE:BaseType = 2;
+
+pub const queueQUEUE_TYPE_BASE:u8 = 0;
+pub const queueQUEUE_TYPE_SET:u8 = 0;
+pub const queueQUEUE_TYPE_MUTEX:u8 = 1;
+pub const queueQUEUE_TYPE_COUNTING_SEMAPHORE:u8 = 2;
+pub const queueQUEUE_TYPE_BINARY_SEMAPHORE:u8 = 3;
+pub const queueQUEUE_TYPE_RECURSIVE_MUTEX:u8 = 4;
+pub const errQUEUE_EMPTY:BaseType = 0;
+pub const errQUEUE_FULL:BaseType = 0;
