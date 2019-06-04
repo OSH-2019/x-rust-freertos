0e2a9f6978ef2192db7f95ec20e7feed3d2b042c
nnnyt <793313994@qq.com>
2019-06-04 21:22:56 +0800

    change the definiton of queue

--- rust_freertos/src/queue_h.rs
+++ rust_freertos/src/queue_h.rs
@@ -13,10 +13,16 @@ pub const queueSEND_TO_BACK: BaseType = 0 ;
 pub const queueSEND_TO_FRONT:BaseType = 1;
 pub const queueOVERWRITE:BaseType = 2;
 
-pub const queueQUEUE_TYPE_BASE:u8 = 0;
-pub const queueQUEUE_TYPE_SET:u8 = 0;
-pub const queueQUEUE_TYPE_MUTEX:u8 = 1;
-pub const queueQUEUE_TYPE_COUNTING_SEMAPHORE:u8 = 2;
-pub const queueQUEUE_TYPE_BINARY_SEMAPHORE:u8 = 3;
-pub const queueQUEUE_TYPE_RECURSIVE_MUTEX:u8 = 4;
+#[derive(PartialEq)]
+pub enum QueueType {
+    Base,
+    Set,
+    Mutex,
+    CountingSemaphtore,
+    BinarySemaphre,
+    RecursiveMutex
+}
+impl Default for QueueType{
+   fn default() -> Self {QueueType::Base}
+ }
 
