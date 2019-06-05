9884b0dfb9b77e6b6baa99cd73b393cc3f16ea60
Roosevelt93 <roosevelt93144@gmail.com>
2019-06-05 03:36:16 +0000

    implement mutex.rs

--- rust_freertos/src/mutex.rs
+++ rust_freertos/src/mutex.rs
@@ -1,9 +1,25 @@
 use crate::queue::*;
+use crate::task_control::*;
+use crate::queue_h::*;
+use crate::*;
 
 #[derive(Default)]
-pub struct Mutex<T>(Queue<T>)
+#[cfg(feature = "configUSE_MUTEXES")]
+pub struct Mutex(Queue<Option<TaskHandle>>);
 
-impl <T>Mutex<T> {
+#[cfg(feature = "configUSE_MUTEXES")]
+impl Mutex {
+    /// # Description
+    /// 
+    /// # Argument
+    /// 
+    /// # Return
+    fn new() -> Self {
+        Mutex::mutex_create()
+    }
+
+
+
     /// # Description
     /// 
     /// # Argument
@@ -11,15 +27,12 @@ impl <T>Mutex<T> {
     /// # Return
     #[cfg(feature = "configUSE_MUTEXES")]
     fn initialise_mutex(&self) {        
-        self.pcHead = 0;
-        self.pcTail = 0;
-        self.ucQueueType = QueueType::Mutex;
+        //self.0.ucQueueType = QueueType::Mutex;
+        //self.0.QueueUnion = 0; //original uxRecursiveCallCount
 
-        self.QueueUnion = 0;
+        traceCREATE_MUTEX!(self);
 
-        traceCREATE_MUTEX!(&self);
-
-        unimplemented!();
+        self.0.send_to_back(None, 0);
     }
 
     /// # Description
@@ -28,9 +41,37 @@ impl <T>Mutex<T> {
     /// 
     /// # Return
     #[cfg(all(feature = "configUSE_MUTEXES", feature = "configSUPPORT_DYNAMIC_ALLOCATION"))]
-    fn queue_creat_mutex(arg: Type) -> RetType {
-        unimplemented!();
+    fn mutex_create() -> Self {
+        let mut mutex: Mutex = Mutex(Queue::new_type(1, QueueType::Mutex));
+        mutex.initialise_mutex();
+        mutex
     }
 
-    s
+    /// # Description
+    /// Note:  This is a good way of determining if the
+	///	calling task is the mutex holder, but not a good way of determining the
+	///	identity of the mutex holder, as the holder may change between the
+	///	following critical section exiting and the function returning
+    /// # Argument
+    /// 
+    /// # Return
+    #[cfg(all(feature = "configUSE_MUTEXES", feature = "INCLUDE_xSemaphoreGetMutexHolder"))]
+    fn get_mutex_holder(& mut self) -> Option<TaskHandle> {
+        let mut mutex_holder: Option<TaskHandle>;
+        taskENTER_CRITICAL!();
+        {
+            mutex_holder  = self.0.peek(0).unwrap(); //tickstowait's value remain discussed
+        }
+        taskEXIT_CRITICAL!();
+        mutex_holder
+    }
+
+    fn get_mutex(&self, Item:TaskHandle) -> (Result<(), QueueError>) {
+        self.0.send_to_back(Some(Item), 0)
+    }
+
+    fn release_mutex(&self) -> (Result<(), QueueError>) {
+        self.0.overwrite(None)
+    }
+
 }
