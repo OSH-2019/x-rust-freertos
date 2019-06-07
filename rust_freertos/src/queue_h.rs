5b921932e6a2b44e023a12f944597e4a9f81851e
Roosevelt93 <roosevelt93144@gmail.com>
2019-06-07 11:48:48 +0000

    modify mutex.rs

--- rust_freertos/src/queue_h.rs
+++ rust_freertos/src/queue_h.rs
@@ -13,6 +13,8 @@ pub const queueSEND_TO_BACK: BaseType = 0 ;
 pub const queueSEND_TO_FRONT:BaseType = 1;
 pub const queueOVERWRITE:BaseType = 2;
 
+pub const semGIVE_BLOCK_TIME: TickType = 0;
+
 #[derive(PartialEq)]
 pub enum QueueType {
     Base,
