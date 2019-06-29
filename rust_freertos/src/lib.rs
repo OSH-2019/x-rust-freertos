df4d73dbff555ecc2d1416d37aad8fe368ef5aa4
Fandahao <fandahao17@mail.ustc.edu.cn>
2019-06-29 12:20:50 +0800

    Update test.

--- rust_freertos/src/lib.rs
+++ rust_freertos/src/lib.rs
@@ -54,31 +54,31 @@ mod tests {
     use std::sync::Arc;
     #[test]
     fn test_queue() {
-        task_global::init();
-
-        use queue::Queue;
-        // 用new方法创建一个长度为10的queue。
-        let q: Queue<u32> = Queue::new(10);
+        use std::fs::File;
+        use simplelog::*;
+        use queue_api::Queue;
 
         // 两个任务共享所有权，所以需Arc包装。
-        let shared_queue = Arc::new(&q);
+        let q_recv = Arc::new(Queue::new(10));
+        let q_sender = Arc::clone(&q_recv);
 
         // 发送数据的任务代码。
         let sender = move || {
-            let v = Arc::clone(&shared_queue);
+            let _ = WriteLogger::init(LevelFilter::Info, Config::default(), File::create("sender.log").unwrap());
             for i in 1..11 {
                 // send方法的参数包括要发送的数据和ticks_to_wait
-                v.send(i, pdMS_TO_TICKS!(50)).unwrap();
+                q_sender.send(i, pdMS_TO_TICKS!(50)).unwrap();
             }
         };
 
         // 接收数据的任务代码。
         let receiver = move || {
-            let v = Arc::clone(&shared_queue);
-            let sum = 0;
+            let _ = WriteLogger::init(LevelFilter::Info, Config::default(), File::create("recv.log").unwrap());
+            let mut sum = 0;
             loop {
                 // receive方法的参数只有ticks_to_wait
-                if let Ok(x) = v.receive(pdMS_TO_TICKS!(300)) {
+                if let Ok(x) = q_recv.receive(pdMS_TO_TICKS!(300)) {
+                    println!("{}", x);
                     sum += x;
                 } else {
                     // 若等待300ms仍未收到数据，则认为发送结束。
@@ -89,12 +89,12 @@ mod tests {
         };
 
         // 创建这两个任务。
-        let sender_task = task_control::TCB::new()
+        let _sender_task = task_control::TCB::new()
                             .name("Sender")
                             .priority(3)
                             .initialise(sender);
 
-        let receiver_task = task_control::TCB::new()
+        let _receiver_task = task_control::TCB::new()
                             .name("Receiver")
                             .priority(3)
                             .initialise(receiver);
