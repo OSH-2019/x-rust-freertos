532b44b9ca56b3fbad9b269029b9c063fc1dc852
Roosevelt93 <roosevelt93144@gmail.com>
2019-07-02 12:45:29 +0000

    pub some function in semaphore.rs

--- rust_freertos/src/semaphore.rs
+++ rust_freertos/src/semaphore.rs
@@ -26,7 +26,7 @@ impl Semaphore {
     }
 
     #[cfg(all(feature = "configUSE_MUTEXES", feature = "INCLUDE_xSemaphoreGetMutexHolder"))]
-    fn get_mutex_holder(&self) -> Option<task_control::TaskHandle> {
+    pub fn get_mutex_holder(&self) -> Option<task_control::TaskHandle> {
         let mut mutex_holder: Option<task_control::TaskHandle>;
         taskENTER_CRITICAL!();
         {
@@ -39,25 +39,25 @@ impl Semaphore {
         mutex_holder
     }
 
-    fn semaphore_take(&self,xBlockTime: TickType) -> Result<Option<TaskHandle>,QueueError> {
+    pub fn semaphore_take(&self,xBlockTime: TickType) -> Result<Option<TaskHandle>,QueueError> {
         unsafe {
             let inner = self.0.get();
             (*inner).queue_generic_receive(xBlockTime, false)
         }
     }
 
-    fn semaphore_give(&self) -> Result<(),QueueError> {
+    pub fn semaphore_give(&self) -> Result<(),QueueError> {
         unsafe {
             let inner = self.0.get();
             (*inner).queue_generic_send(None,semGIVE_BLOCK_TIME,queueSEND_TO_BACK)
         }
     }
 
-    fn create_binary () -> Self {
+    pub fn create_binary () -> Self {
         Semaphore(UnsafeCell::new(QueueDefinition::new(1,QueueType::BinarySemaphore)))
     }
     
-    fn create_counting(max_count: UBaseType,initial_count:UBaseType) -> Self {
+    pub fn create_counting(max_count: UBaseType,initial_count:UBaseType) -> Self {
         let mut counting_semphr = Semaphore(UnsafeCell::new(QueueDefinition::new(max_count,QueueType::CountingSemaphore)));
         unsafe {
             let inner = counting_semphr.0.get();
@@ -68,11 +68,11 @@ impl Semaphore {
     }
 
     //#[cfg(feature = "configUSE_RECURSIVE_MUTEXES")]
-    fn create_recursive_mutex() -> Self {
+    pub fn create_recursive_mutex() -> Self {
         Semaphore(UnsafeCell::new(QueueDefinition::new(1,QueueType::RecursiveMutex)))
     }
 
-    fn give_recursive(&self) -> bool {
+    pub fn give_recursive(&self) -> bool {
         unsafe {
             let inner = self.0.get();
             if  (*inner).transed_task_handle_for_mutex().unwrap().clone() == get_current_task_handle!() {
@@ -93,7 +93,7 @@ impl Semaphore {
         }
     }
 
-    fn take_recursive(&self, ticks_to_wait:TickType) -> bool {
+    pub fn take_recursive(&self, ticks_to_wait:TickType) -> bool {
         let mut xReturn:bool = false;
         unsafe {
             let inner = self.0.get();
